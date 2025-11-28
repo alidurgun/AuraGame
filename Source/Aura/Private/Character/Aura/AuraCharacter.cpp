033d4e42b64038028004617d8a8af5fcc96beb19
Ali Durgun <ali.durgun1994@gmail.com>
2025-11-28 19:41:06 +0100

    Player Mode added. Camera settings for the player.

--- Source/Aura/Private/Character/Aura/AuraCharacter.cpp
+++ Source/Aura/Private/Character/Aura/AuraCharacter.cpp
@@ -3,7 +3,20 @@
 
 #include "Character/Aura/AuraCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
 AAuraCharacter::AAuraCharacter()
 {
-	
+	// to turn character's face to the direction that we are moving.
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	// Turn speed to the new direction.
+	GetCharacterMovement()->RotationRate = FRotator(0.0f,400.0f,0.0f);
+	// Movement will be constrained to plane.
+	GetCharacterMovement()->bConstrainToPlane = true;
+	// Updated component will be snapped to the plane when first attached.
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
 }
---
+++ Source/Aura/Private/GameMode/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Project has been managed by Ali Durgun
+
+
+#include "GameMode/AuraGameModeBase.h"
+
