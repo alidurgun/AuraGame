81c199de86c0bcd7afe34591b3e1579538521f0a
Ali Durgun <ali.durgun1994@gmail.com>
2026-02-22 16:45:30 +0100

    Motion Warping

--- Source/Aura/Public/Interface/CombatInterface.h
+++ Source/Aura/Public/Interface/CombatInterface.h
@@ -6,8 +6,9 @@
 #include "UObject/Interface.h"
 #include "CombatInterface.generated.h"
 
+// BlueprintType => In order to cast to CombatInterface in the BP side.
 // This class does not need to be modified.
-UINTERFACE(MinimalAPI)
+UINTERFACE(MinimalAPI, BlueprintType)
 class UCombatInterface : public UInterface
 {
 	GENERATED_BODY()
@@ -26,4 +27,9 @@ public:
 	virtual int32 GetPlayerLevel() const { return 0; }
 	// To get the location for the tip of the weapon.
 	virtual const FVector GetCombatSocketLocation() const { return FVector::ZeroVector; }
+
+	// When we use BlueprintImplementableEvent we don't use virtual keyword.
+	// It will show as "Event Update Facing Target" in BP side.(BlueprintImplementableEvent used)
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void UpdateFacingTarget(const FVector& Target);
 };
